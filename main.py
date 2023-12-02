bf12cf155131aad441d6e5d7896171b9f23eb245
chenaa083 <131934827+chenaa083@users.noreply.github.com>
2023-12-02 22:35:14 +0800

    test
    test1

---
+++ main.py
@@ -0,0 +1,20 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import torch as th
+print(th.cuda.is_available()) # 查看CUDA是否可用
+print(th.cuda.device_count()) # 查看可用的CUDA数量
+print(th.version.cuda) # 查看CUDA的版本号
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
